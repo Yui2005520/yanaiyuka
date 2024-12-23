d0dfa14138bc198f61d570c93ce8c0c80de42cf3
Yui2005520 <24hs212@s.bgu.ac.jp>
2024-12-23 14:39:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
             var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "凶"; // デフォルトの値
 
-            if (colour === "黒" && number === 0) {
+            if (colour === "モコモコしていて、垂れ耳な小型犬" トイプードル　原産国　フランス&& number === 0) {
                 result = "吉";
-            } else if (colour === "黒" && number === 1) {
+            } else if (colour === "立ち耳の小型犬はチワワ　原産国　メキシコ" && number === 1) {
                 result = "末吉";
             } else if (colour === "黒" && number === 2) {
                 result = "小吉";
